931ee762f3de25865cd2bc7492ff62469e0e0f0f
pangpang <pang4960@163.com>
2018-07-11 20:17:17 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by pp on 2018/7/10.
+ */
+/*
+ 对象: banner
+ 属性: imgs, index , imgW , time , interval , timer
+ 行为: init , rightClick , leftClick , stopAuto , startAuto , addAuto , animate , circleClick
+ */
+$(function () {
+
+})
