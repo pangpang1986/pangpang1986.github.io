76ac777f77475384e8486ee9c0dad335a73b9fdc
pangpang <pang4960@163.com>
2018-07-29 14:08:42 +0800

    date

--- js/index.js
+++ js/index.js
@@ -7,5 +7,17 @@
  行为: init , rightClick , leftClick , stopAuto , startAuto , addAuto , animate , circleClick
  */
 $(function () {
-
+    function javascriptFn(){
+        var javascript = $("#javascrit p");
+        var javascriptInner = $("#javascriptInner");
+        var css3 = $("#css3");
+        var css3Inner = $(".css3Inner");
+        css3.click(function(){
+            css3Inner.slideToggle(500);
+        });
+        javascript.click(function(){
+            javascriptInner.slideToggle(500);
+        });
+    }
+    javascriptFn()
 })
